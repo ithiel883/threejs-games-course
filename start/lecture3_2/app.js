579b8c630d3732e3cd718c4c4890ede876b7d5c1
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-26 22:11:36 +0200

    please work

--- start/lecture3_2/app.js
+++ start/lecture3_2/app.js
@@ -6,6 +6,12 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
     
+		this.camera = new THREE.PerspectiveCamera(60, window.innerWidth, window.innerHeight, 0.1, 100)
+
+		this.camera.position.set()
+
+
+
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
